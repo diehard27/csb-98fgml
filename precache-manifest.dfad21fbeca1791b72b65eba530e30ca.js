a04b6fb5de5797b7a8329bd3e6ca6c9c74691e97
Code Sandbox <hello@codesandbox.io>
2024-06-26 03:14:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dfad21fbeca1791b72b65eba530e30ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb267af48ebfe603706d530a9803d485",
+    "url": "https://.github.io/csb-98fgml/index.html"
+  },
+  {
+    "revision": "c68c316c08e2e4e2b2c2",
+    "url": "https://.github.io/csb-98fgml/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "d28c1e12e0cce1dc01ce",
+    "url": "https://.github.io/csb-98fgml/static/js/2.9c123ed4.chunk.js"
+  },
+  {
+    "revision": "c68c316c08e2e4e2b2c2",
+    "url": "https://.github.io/csb-98fgml/static/js/main.bd84a2ad.chunk.js"
+  },
+  {
+    "revision": "514cf41e3600522892eb",
+    "url": "https://.github.io/csb-98fgml/static/js/runtime~main.fab96274.js"
+  }
+]);
